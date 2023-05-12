638ad7cefb282355e2bfdf7a711d5a297a0c8444
K.Veronicah <50793802+KilisioVee@users.noreply.github.com>
2023-05-12 13:43:59 +0300

    Updated API_KEY

---
+++ OrderTrackingPage.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+class OrderTrackingPage extends StatefulWidget {
+  const OrderTrackingPage({Key? key}) : super(key: key);
+  @override
+  State<OrderTrackingPage> createState() => OrderTrackingPageState();
+}
+class OrderTrackingPageState extends State<OrderTrackingPage> {
+  final Completer<GoogleMapController> _controller = Completer();
+  static const LatLng sourceLocation = LatLng(-1.258174, 36.818733);
+  static const LatLng destination = LatLng(-1.25887,36.80528);
+  List<LatLng> polylineCoordinates = [];
+  String google_api_key='YOUR API_KEY HERE';
+  LocationData? currentLocation;
+  BitmapDescriptor sourceIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor destinationIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor currentLocationIcon = BitmapDescriptor.defaultMarker;
+
+  @override
+  void initState() {
+    getCurrentLocation();
+    getPolyPoints();
+    setCustomMarkerIcon();
+
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+
+
+    return Scaffold(
+      body: currentLocation == null
+          ? const Center(child: Text("Loading"))
+          : GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+              currentLocation!.latitude!, currentLocation!.longitude!),
+          zoom: 13.5,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("currentLocation"),
+            icon: currentLocationIcon,
+            position: LatLng(
+                currentLocation!.latitude!, currentLocation!.longitude!),
+          ),
+          Marker(
+            markerId: const MarkerId("source"),
+            icon: sourceIcon,
+            position: sourceLocation,
+          ),
+          Marker(
+            markerId: MarkerId("destination"),
+            icon: destinationIcon,
+            position: destination,
+          ),
+        },
+
+        onMapCreated: (mapController) {
+          _controller.complete(mapController);
+        },
+        polylines: {
+          Polyline(
+            polylineId: const PolylineId("route"),
+            points: polylineCoordinates,
+            color: const Color(0xFF7B61FF),
+            width: 6,
+          ),
+        },
+
+
+
+      ),
+    );
+  }
+  //Draw Route Direction
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      google_api_key, // Your Google Map Key
+      PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+      PointLatLng(destination.latitude, destination.longitude),
+    );
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+            (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+    }
+  }
+  //Real-time Location Updates on Map
+  void getCurrentLocation() async {
+    Location location = Location();
+
+
+    await location.getLocation().then(
+          (location) {
+        currentLocation = location;
+        setState(() {
+          currentLocation = location;
+
+
+        });
+      },
+    ).catchError((e) {
+      debugPrint(e);
+    });
+    GoogleMapController googleMapController = await _controller.future;
+    location.onLocationChanged.listen(
+          (newLoc) {
+        currentLocation = newLoc;
+        googleMapController.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              zoom: 13.5,
+              target: LatLng(
+                newLoc.latitude!,
+                newLoc.longitude!,
+              ),
+            ),
+          ),
+        );
+        setState(() {});
+      },
+    );
+  }
+  //Add custom Marker/Pin
+  void setCustomMarkerIcon() {
+    BitmapDescriptor.fromAssetImage(
+        ImageConfiguration.empty, "assets/Pin_source.png")
+        .then(
+          (icon) {
+        sourceIcon = icon;
+      },
+    );
+    BitmapDescriptor.fromAssetImage(
+        ImageConfiguration.empty, "assets/Pin_destination.png")
+        .then(
+          (icon) {
+        destinationIcon = icon;
+      },
+    );
+    BitmapDescriptor.fromAssetImage(
+        ImageConfiguration.empty, "assets/Badge.png")
+        .then(
+          (icon) {
+        currentLocationIcon = icon;
+      },
+    );
+  }
+}
